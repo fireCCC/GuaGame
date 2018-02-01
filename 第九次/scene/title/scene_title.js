7aa3da970d9ad24ae9f41d31a500415d7f10a904
fireCCC <31002654+fireCCC@users.noreply.github.com>
2018-02-01 23:50:05 +0800

    Add files via upload

---
+++ 第九次/scene/title/scene_title.js
@@ -0,0 +1,102 @@
+class GuaLabel {
+    constructor(game, text) {
+        this.game = game
+        this.text = text
+    }
+    static new(game, text) {
+        return new this(game, text)
+    }
+    draw() {
+        this.game.context.fillText(this.text, 100, 100)
+    }
+    update() {
+
+    }
+}
+
+class GuaParticle extends GuaImage {
+    constructor(game) {
+        super(game, 'fire')
+        this.setup()
+    }
+    setup() {
+        this.life = 20
+    }
+    init(x, y, vx, vy) {
+        this.x = x
+        this.y = y
+        this.vx = vx
+        this.vy = vy
+    }
+    update() {
+        this.life--
+        this.x += this.vx
+        this.y += this.vy
+        var factor = 0.01
+        this.vx += factor * this.vx
+        this.vy += factor * this.vy
+    }
+}
+
+class GuaParticleSystem {
+    constructor(game) {
+        this.game = game
+        this.setup()
+    }
+    static new(game) {
+        return new this(game)
+    }
+    setup() {
+        this.duration = 50
+        this.x = 150
+        this.y = 100
+        this.numberOfParticles = 100
+        this.particles = []
+    }
+    update() {
+        this.duration--
+        
+        if(this.particles.length < this.numberOfParticles) {
+            
+            var p = GuaParticle.new(this.game)
+            var s = 2
+            var vx = randomBetween(-s, s)
+            var vy = randomBetween(-s, s)
+            p.init(this.x, this.y, vx, vy)
+            this.particles.push(p)
+        }
+
+        for (var p of this.particles) {
+            p.update()
+        }
+
+        this.particles = this.particles.filter(p => p.life > 0)
+    }
+    draw() {
+        if(this.duration < 0) {
+            return 
+        }
+        for (var p of this.particles) {
+            p.draw()
+        }
+    }
+    
+}
+
+class SceneTitle extends GuaScene {
+    constructor(game) {
+        super(game)
+        //初始化
+        var label = GuaLabel.new(game, 'hello')
+        this.addElement(label)
+
+        var ps = GuaParticleSystem.new(game)
+
+        this.addElement(ps)
+
+    }
+    static new(game) {
+        var i = new this(game)
+        return i
+    }
+}
